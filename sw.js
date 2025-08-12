e9766e3122e310ed76a7c44cc49385e70776bcb6
HarunTekin1 <h.tekin1807@gmail.com>
2025-08-12 23:38:44 +0200

    chore: standardize logo filename (vcan-logo.jpg) and update references

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 const CACHE_NAME = 'vcan-static-v1';
 const ASSETS = [
   'index.html','styles.css','app.js','manifest.json',
-  'assets/VCan LOGO.jpg','assets/icon-192.png','assets/icon-512.png','assets/logo.svg'
+  'assets/vcan-logo.jpg','assets/icon-192.png','assets/icon-512.png'
 ];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
