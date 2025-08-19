9f1288c4be70cc2cd3447032c1dad904669a14a0
HarunTekin1 <h.tekin1807@gmail.com>
2025-08-19 22:21:16 +0200

    partner: nudge left-aligned texts right (indent lists/steps slightly); bump SW cache v110

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Basic offline cache for static assets
-const CACHE_NAME = 'vcan-static-v109';
+const CACHE_NAME = 'vcan-static-v110';
 const ASSETS = [
   'index.html','vision.html','kontakt.html','impressum.html','datenschutz.html','agb.html','offline.html','admin.html','fuer-user.html',
   'partner.html','en/index.html',
